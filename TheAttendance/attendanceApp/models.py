200b582923b66bc99f5dd50576af9e9241c39f8e
Lotus <cmadisonne@gmail.com>
2018-12-03 15:07:36 -0600

    inProgress

--- TheAttendance/attendanceApp/models.py
+++ TheAttendance/attendanceApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, timezone
 
-# Create your models here.
+class attendanceModel(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    clockIn = models.DateTimeField(datetime.now)
+    clockOut = models.DateTimeField(datetime.now)
+
+    def __unicode__(self):
+        return self.username
