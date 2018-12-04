727e6d24c4b71761ce2a87b7441756b8e954df9b
Jordan Parker <parkerj4321@gmail.com>
2018-12-04 15:17:42 -0600

    Uh small things

--- TheAttendance/attendanceApp/views.py
+++ TheAttendance/attendanceApp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. This is the Attendance App")
+    return render(request, 'attendanceApp/index.html', )
