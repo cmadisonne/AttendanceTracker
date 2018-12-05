81e7c5c7db8301802f078da2b53bb88ef65ca865
Lotus <cmadisonne@gmail.com>
2018-12-05 12:49:52 -0600

    WorkingForms

--- TheAttendance/attendanceApp/views.py
+++ TheAttendance/attendanceApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import attendanceModel
-from .forms import attendanceForm
+from .forms import clockInForm, clockOutForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
@@ -11,4 +11,23 @@ def index(request):
     return render(request, 'attendanceApp/index.html', context)
 
 def clockIn(request):
-    return render(request, 'attendanceApp/')
+    if request.method == 'POST':
+        form = clockInForm(request.POST)
+        if form.is_valid():
+            newForm = form.save(commit=False)
+            newForm.save()
+            return redirect('index')
+    else:
+        form = clockInForm()
+        return render(request, 'attendanceApp/clockIn.html', {'form':form})
+
+def clockOut(request):
+    if request.method == 'POST':
+        form = clockOutForm(request.POST)
+        if form.is_valid():
+            newForm = form.save(commit=False)
+            newForm.save()
+            return redirect('index')
+    else:
+        form = clockOutForm()
+        return render(request, 'attendanceApp/clockOut.html', {'form':form})
