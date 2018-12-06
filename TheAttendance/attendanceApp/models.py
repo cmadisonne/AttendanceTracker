708dbce76c3e4a576e309aeb2794771cbc75a297
Jordan Parker <parkerj4321@gmail.com>
2018-12-06 16:18:08 -0600

    Tried to fix the time

--- TheAttendance/attendanceApp/models.py
+++ TheAttendance/attendanceApp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime, timezone
+from datetime import datetime
+from django.utils import timezone
 
 class clockIn(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     password = models.CharField(max_length=30)
-    clockIn = models.DateTimeField(default=datetime.now())
+    clockIn = models.DateTimeField(default=timezone.now, null=True, blank=True)
 
     def __unicode__(self):
         return self.username
@@ -13,7 +14,7 @@ class clockIn(models.Model):
 class clockOut(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     password = models.CharField(max_length=30)
-    clockOut = models.DateTimeField(default=datetime.now())
+    clockOut = models.DateTimeField(default=timezone.now, null=True, blank=True)
 
     def __unicode__(self):
         return self.username
